47c1409a968e0f3d230bc2fad8b8960f213e0069
tym0027 <tarupprecht@gmail.com>
2020-04-14 18:09:27 -0400

    creating checkpoint before conveerting to encoder decoder structre

--- NMT.py
+++ NMT.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+import time
+from datetime import datetime
+
 import json
 import os
 
@@ -10,6 +13,15 @@ from bleu import *
 chencherry = SmoothingFunction()
 
 torch.manual_seed(1)
+
+def log(s):
+    dateTimeObj = datetime.now()
+    timestamp = dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S)")
+
+    f = open('logfile/log.txt', 'a')
+    f.write(timestamp + ": " + s.replace("\n","\n" + timestamp + ":") + '\n')
+
+    f.close()
 
 class NMT(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
@@ -45,6 +57,7 @@ def preprocess(rootDir, sampleSize, englishDictionary, englishTraining, germanDi
         trainingData, englishDictionaryNew, uselines = savedEngData[0], savedEngData[1], savedEngData[2]
     
     print("\t[ OK ] english data prepared...")
+    log("\t[ OK ] english data prepared...")
 
     if not os.path.exists(rootDir + "data-de-" + str(sampleSize) + ".json"):
         trainingLabels, germanDictionaryNew, uselines = loadData(germanDictionary, germanEmbedding, uselines)
@@ -56,6 +69,7 @@ def preprocess(rootDir, sampleSize, englishDictionary, englishTraining, germanDi
             savedGerData = json.load(file)
         trainingLabels, germanDictionaryNew, uselines = savedGerData[0], savedGerData[1], savedGerData[2]
     print("\t[ OK ] german data prepared...")
+    log("\t[ OK ] german data prepared...")
 
     '''
     print("consolidating lists...")
@@ -66,7 +80,7 @@ def preprocess(rootDir, sampleSize, englishDictionary, englishTraining, germanDi
     '''
 
     print("\n\t[ OK ] loaded data...")
-
+    log("\n\t[ OK ] loaded data...\n")
     print()
 
     return savedEngData, savedGerData 
@@ -94,10 +108,15 @@ def convert(dictionary, data):
 
     # print(data.shape)
     # print(m.shape)
-    for itm in range(1, data.shape[0] - 1):
+    for itm in range(0, data.shape[0]):
         # print(torch.max(data[itm,:], dim=0)[1].detach().item())
         s += str(dictionary[torch.max(data[itm,:], dim=0)[1].detach().item()]) + " "
     return s
+
+
+def saveModel(model, path):
+    torch.save(model.state_dict(), path)
+
 
 def loadData(dictionary, lines, uselines):
     new_data = []
@@ -131,8 +150,9 @@ def loadData(dictionary, lines, uselines):
 
 ### Prepare data
 print("preparing training data...")
+log("preparing training data...")
 
-sampleSize = 10
+sampleSize = 1
 rootDir = "./datasets/"
 englishDictionary = open(rootDir + "./vocab.50K.en.txt", 'r').read().lower().split("\n")
 germanDictionary = open(rootDir + "./vocab.50K.de.txt", 'r').read().lower().split("\n")
@@ -151,24 +171,28 @@ validationSet = ["newstest2012","newstest2013","newstest2014","newstest2015"]
 englishValidation = []
 germanValidation = []
 print("preparing validation data...")
+log("preparing validation data...")
 
 for obj in validationSet:
     englishValidation = englishValidation + open(rootDir + "./" + obj + ".en.txt", 'r').read().lower().split("\n")
     germanValidation = germanValidation + open(rootDir + "./" + obj + ".de.txt", 'r').read().lower().split("\n")
 
     print("\t[ OK ] ", obj)
+    log("\t[ OK ] " + str(obj))
 
 savedEngVal = loadData(englishDictionary, englishValidation, range(0, len(englishValidation)))
 savedGerVal = loadData(germanDictionary, germanValidation, range(0, len(englishValidation)))
 
 print("\n\t[ OK ] loaded validation data")
+log("\n\t[ OK ] loaded validation data")
 
 # These will usually be more like 32 or 64 dimensional.
 # We will keep them small, so we can see how the weights change as we train.
-EMBEDDING_DIM = 64 # 6
-HIDDEN_DIM = 64 # 6
+EMBEDDING_DIM = 128 # 128 # 6
+HIDDEN_DIM = 128 # 128 # 6
 
 print("\npreparing model...")
+log("\npreparing model...")
 
 model = NMT(EMBEDDING_DIM, HIDDEN_DIM, len(savedEngData[0]), len(savedEngData[1])).cuda()
 loss_function = nn.NLLLoss()
@@ -176,9 +200,10 @@ optimizer = optim.SGD(model.parameters(), lr=0.1)
 
 
 #### Divide data for epochs
-num_epochs = 30
+num_epochs = int(300 / sampleSize)
 epoch_data = list(split(savedEngData[0], num_epochs))
 print(len(epoch_data), "epochs")
+log(str(len(epoch_data)) + " epochs...")
 
 # See what the scores are before training
 # Note that element i,j of the output is the score for tag j for word i.
@@ -190,12 +215,17 @@ with torch.no_grad():
     print(tag_scores)
 '''
 
+best_bleu = 0
 index = 0
 print("starting...")
+log("starting...")
 for epoch in range(num_epochs):  # again, normally you would NOT do 300 epochs, it is toy data
     
     _loss = 0
+    _bleu = 0
     model.train(True)
+    predictions = []
+    groundTruth = []
 
     # Training
     for sentence in epoch_data[epoch]:
@@ -217,27 +247,40 @@ for epoch in range(num_epochs):  # again, normally you would NOT do 300 epochs, 
         # Step 4. Compute the loss, gradients, and update the parameters by
         #  calling optimizer.step()
         
-        loss = loss_function(tag_scores, targets)
-        
+        # b = sentence_bleu([savedGerData[0][index][0]], convert(germanDictionary, tag_scores).split(" "), smoothing_function=chencherry.method1)
+        # _bleu += b
 
+        predictions.append(convert(germanDictionary, tag_scores).split(" "))
+        groundTruth.append([["<s>"] + savedGerData[0][index][0] + ["</s>"]])
+        
+        # print("\ntest...")
+        # print(convert(germanDictionary, tag_scores).split(" "))
+        # print([["<s>"] + savedGerData[0][index][0] + ["</s>"]])        
+
+        # loss = .5 * loss_function(tag_scores, targets) * (2*(1 - b))**2
+        loss = loss_function(tag_scores, targets)
         _loss += loss.item()
+
         '''
         if index % 100 == 0:
             print("\t",index,")", str(loss.item()))
         '''
         loss.backward()
-
         optimizer.step()
 
         index += 1
-    
-    # print("Train ", epoch, " ) loss: ", str(_loss/len(epoch_data[epoch])))
+   
+    # calculate bleu score for training
+    train_bleu = corpus_bleu(groundTruth, predictions, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=chencherry.method1, auto_reweigh=False,)
+    # train_bleu = _bleu
 
     # Validation
-    # _loss = 0
     model.train(False)
     model.eval()
     _bleu = 0
+
+    groundTruth = []
+    predictions = []
 
     val_index = 0;
     for sentence in savedEngVal[0]:
@@ -247,14 +290,28 @@ for epoch in range(num_epochs):  # again, normally you would NOT do 300 epochs, 
         with torch.no_grad():
             tag_scores = model(sentence_in)
 
+        predictions.append(convert(germanDictionary, tag_scores).split(" "))
+        groundTruth.append([["<s>"] + savedGerVal[0][val_index][0] + ["</s>"]])
+
         # print("sentence: ", " ".join(sentence[0]))
         # print("ground truth: ", " ".join(savedGerVal[0][val_index][0]))
         # print("predicted: ", convert(germanDictionary, tag_scores))
 
-        _bleu += sentence_bleu([savedGerVal[0][val_index][0]], convert(germanDictionary, tag_scores).split(" "), smoothing_function=chencherry.method1)
+        # _bleu += sentence_bleu([savedGerVal[0][val_index][0]], convert(germanDictionary, tag_scores).split(" "), smoothing_function=chencherry.method1)
         val_index += 1
 
-    print(epoch, ") training (loss): ", str(_loss/len(epoch_data[epoch])), "  | validation (bleu): ", _bleu/len(savedEngVal[0])) # , " BLEU: ", b)
+    _bleu = corpus_bleu(groundTruth, predictions, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=chencherry.method1, auto_reweigh=False,) 
+
+    po = str(epoch) + " ) training (loss): " + str(_loss/len(epoch_data[epoch])) + "  (bleu): " + str(train_bleu) + " | validation (bleu): " +  str(_bleu)  
+    print(epoch, ") training (loss): ", str(_loss/len(epoch_data[epoch])), "  (bleu): ", str(train_bleu), " | validation (bleu): ", _bleu) # , " BLEU: ", b)
+    log(po)
+
+    if _bleu > best_bleu:
+        saveModel(model, "./model/english_to_german_b" + str(_bleu) + "-e" + str(epoch) + ".pth")
+        print("saving model...")
+        log("saving model...")
+        best_bleu = _bleu
+
 
 # See what the scores are after training
 '''
